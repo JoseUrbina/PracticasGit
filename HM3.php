17de07cd25881b377ebe883b4f67c3fd90eb7cac
Jose Urbina <joseantonioug-09@hotmail.com>
2016-11-10 16:40:23 -0600

    Practicas1
    Archivos subidos desde mi PC local

---
+++ HM3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Prueba</title>
+</head>
+<body>
+	<?php 
+		echo "Hola Mundo";
+
+		echo "Hola Mundo 2";
+		echo "Hola Mundo 3";
+
+	?>
+</body>
+</html>
